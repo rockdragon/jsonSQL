350d132ff80cfe16f6f9ff1554d767ea0890bec6
Locke <moyerock@gmail.com>
2016-05-26 19:19:05 +0800

    feat: supply comprison operator.

---
+++ examples/comparative_query.js
@@ -0,0 +1,24 @@
+var dataSource = {
+  '4866102f06de4f38bc30592e001cf423': {
+    name: 'Tom',
+    age: 33,
+    birth: new Date(2016, 1, 1)
+  },
+  'd07872f7d2e8447bbe874bbfd3fb0296': {
+    name: 'Jack',
+    age: 18,
+    birth: new Date(2015, 1, 1)
+  },
+  'd935869b80f542a9bf3f6a59d4f635f1': {
+    name: 'Peter',
+    age: 26,
+    birth: new Date(2014, 1, 1)
+  },
+};
+
+var Query = require('../index');
+//var res = Query(dataSource, '* where age<=30');
+//console.log('======RESULT 1\n',res);
+
+var res = Query(dataSource, '* where birth.valueOf()>' + new Date(2015,1,1).valueOf());
+console.log('======RESULT 2\n',res);
--- index.js
+++ index.js
@@ -1,237 +1,284 @@
 var AND = '&&'
-    , OR = '||'
-    , AND_STR = 'and'
-    , OR_STR = 'or'
-    , NOT = '!'
-    , EQUAL = '='
-    , LIKE = '~'
-    , NOTEQUAL = NOT + EQUAL
-    , NOTLIKE = NOT + LIKE
-    , WILDCARD = '*'
-    , COMMA = ','
-    , DELIMITER = '.'
-    , LEFT = '('
-    , RIGHT = ')'
-    , WHERE = 'where'
-    , synopsis = {
-        pathway: [],
-        groups: {}
-    }
-    , AST = {}
-    , options = {};
+  , OR = '||'
+  , AND_STR = 'and'
+  , OR_STR = 'or'
+  , NOT = '!'
+  , EQUAL = '='
+  , LIKE = '~'
+  , NOTEQUAL = NOT + EQUAL
+  , NOTLIKE = NOT + LIKE
+  , GT = '>'
+  , GE = '>='
+  , LT = '<'
+  , LE = '<='
+  , WILDCARD = '*'
+  , COMMA = ','
+  , DELIMITER = '.'
+  , LEFT = '('
+  , RIGHT = ')'
+  , WHERE = 'where'
+  , synopsis = {
+  pathway: [],
+  groups: {}
+}
+  , AST = {}
+  , options = {};
+
+var print = console.log;
 
 function Tokenize(query) {
-    var parts = __splitTrim(query, WHERE);
-    var pathway = parts[0];
-    var where = parts[1];
+  var parts = __splitTrim(query, WHERE);
+  var pathway = parts[0];
+  var where = parts[1];
 
-    synopsis.pathway = __splitTrim(pathway, COMMA);
-    for (var i = 0, len = synopsis.pathway.length; i < len; i++) {
-        synopsis.pathway[i] = __splitTrim(synopsis.pathway[i], DELIMITER);
-        if (synopsis.pathway[i][0] == WILDCARD)
-            synopsis.pathway[i].shift();
-        if(synopsis.pathway[i].length === 0)
-            synopsis.pathway.splice(i, 1);
-    }
+  synopsis.pathway = __splitTrim(pathway, COMMA);
+  for (var i = 0, len = synopsis.pathway.length; i < len; i++) {
+    synopsis.pathway[i] = __splitTrim(synopsis.pathway[i], DELIMITER);
+    if (synopsis.pathway[i][0] == WILDCARD)
+      synopsis.pathway[i].shift();
+    if (synopsis.pathway[i].length === 0)
+      synopsis.pathway.splice(i, 1);
+  }
 
-    var lastLeft = -1,
-        lastRight = -1,
-        current = 0;
-    while (current < where.length) {
-        if (where[current] === LEFT) {
-            lastLeft = current;
-        } else if (where[current] === RIGHT) {
-            lastRight = current;
-            if (lastRight > lastLeft && lastLeft !== -1) {
-                var k = 'gr' + '_' + new Date().getTime();
-                synopsis.groups[k] = where.substring(lastLeft + 1, lastRight);
-                where = where.replace(LEFT + synopsis.groups[k] + RIGHT, k);
-                current = -1;
-            }
-        }
-        current += 1;
+  var lastLeft = -1,
+    lastRight = -1,
+    current = 0;
+  while (current < where.length) {
+    if (where[current] === LEFT) {
+      lastLeft = current;
+    } else if (where[current] === RIGHT) {
+      lastRight = current;
+      if (lastRight > lastLeft && lastLeft !== -1) {
+        var k = 'gr' + '_' + new Date().getTime();
+        synopsis.groups[k] = where.substring(lastLeft + 1, lastRight);
+        where = where.replace(LEFT + synopsis.groups[k] + RIGHT, k);
+        current = -1;
+      }
     }
-    LogicalGrouping(AST, where);
+    current += 1;
+  }
+  LogicalGrouping(AST, where);
 }
 function LogicalGrouping(current, where) {
-    var lastAnd = __findIndex(where, AND),
-        lastOr = __findIndex(where, OR);
+  var lastAnd = __findIndex(where, AND),
+    lastOr = __findIndex(where, OR);
 
-    if (lastAnd !== Number.MAX_VALUE || lastOr !== Number.MAX_VALUE) {
-        if (lastAnd < lastOr) {
-            current.and = current.and || [];
-            var parts = __splitTrim(where, AND);
-            current.and.push(parts[0]);
-            LogicalGrouping(current.and, parts[1]);
-        } else {
-            current.or = current.or || [];
-            var parts = __splitTrim(where, OR);
-            current.or.push(parts[0]);
-            LogicalGrouping(current.or, parts[1]);
-        }
+  if (lastAnd !== Number.MAX_VALUE || lastOr !== Number.MAX_VALUE) {
+    if (lastAnd < lastOr) {
+      current.and = current.and || [];
+      var parts = __splitTrim(where, AND);
+      current.and.push(parts[0]);
+      LogicalGrouping(current.and, parts[1]);
     } else {
-        if (synopsis.groups[where]) {
-            where = synopsis.groups[where];
-            LogicalGrouping(current, where);
-        } else {
-            if (Array.isArray(current))
-                current.push(where);
-            else
-                current.or = [where];
-            ExtractExpression(AST.or ? AST.or : AST.and)
-        }
+      current.or = current.or || [];
+      var parts = __splitTrim(where, OR);
+      current.or.push(parts[0]);
+      LogicalGrouping(current.or, parts[1]);
     }
+  } else {
+    if (synopsis.groups[where]) {
+      where = synopsis.groups[where];
+      LogicalGrouping(current, where);
+    } else {
+      if (Array.isArray(current))
+        current.push(where);
+      else
+        current.or = [where];
+      ExtractExpression(AST.or ? AST.or : AST.and)
+    }
+  }
 }
 function ExtractExpression(logicalGroup) {
-    for (var k in logicalGroup) {
-        if (logicalGroup.hasOwnProperty(k)) {
-            if (Array.isArray(logicalGroup[k])) {
-                ExtractExpression(logicalGroup[k]);
-            }
-            else if (typeof logicalGroup[k] === 'string') {
-                if (__contains(logicalGroup[k], NOTEQUAL)) {
-                    var parts = __splitTrim(logicalGroup[k], NOTEQUAL);
-                    logicalGroup[k] = {
-                        ne: [
-                            parts[0],
-                            parts[1]
-                        ]
-                    };
-                } else if (__contains(logicalGroup[k], NOTLIKE)) {
-                    var parts = __splitTrim(logicalGroup[k], NOTLIKE);
-                    logicalGroup[k] = {
-                        nreq: [
-                            parts[0],
-                            parts[1]
-                        ]
-                    };
-                } else if (__contains(logicalGroup[k], EQUAL)) {
-                    var parts = __splitTrim(logicalGroup[k], EQUAL);
-                    logicalGroup[k] = {
-                        eq: [
-                            parts[0],
-                            parts[1]
-                        ]
-                    };
-                } else if (__contains(logicalGroup[k], LIKE)) {
-                    var parts = __splitTrim(logicalGroup[k], LIKE);
-                    logicalGroup[k] = { // rough eq
-                        req: [
-                            parts[0],
-                            parts[1]
-                        ]
-                    };
-                }
-            }
+  for (var k in logicalGroup) {
+    if (logicalGroup.hasOwnProperty(k)) {
+      if (Array.isArray(logicalGroup[k])) {
+        ExtractExpression(logicalGroup[k]);
+      }
+      else if (typeof logicalGroup[k] === 'string') {
+        if (__contains(logicalGroup[k], NOTEQUAL)) {
+          var parts = __splitTrim(logicalGroup[k], NOTEQUAL);
+          logicalGroup[k] = {
+            ne: [
+              parts[0],
+              parts[1]
+            ]
+          };
+        } else if (__contains(logicalGroup[k], NOTLIKE)) {
+          var parts = __splitTrim(logicalGroup[k], NOTLIKE);
+          logicalGroup[k] = {
+            nreq: [
+              parts[0],
+              parts[1]
+            ]
+          };
+        } else if (__contains(logicalGroup[k], LIKE)) {
+          var parts = __splitTrim(logicalGroup[k], LIKE);
+          logicalGroup[k] = { // rough eq
+            req: [
+              parts[0],
+              parts[1]
+            ]
+          };
+        } else if (__contains(logicalGroup[k], GE)) {
+          var parts = __splitTrim(logicalGroup[k], GE);
+          logicalGroup[k] = { // greater than or equal
+            ge: [
+              parts[0],
+              parts[1]
+            ]
+          };
+        } else if (__contains(logicalGroup[k], GT)) {
+          var parts = __splitTrim(logicalGroup[k], GT);
+          logicalGroup[k] = { // greater than
+            gt: [
+              parts[0],
+              parts[1]
+            ]
+          };
+        } else if (__contains(logicalGroup[k], LE)) {
+          var parts = __splitTrim(logicalGroup[k], LE);
+          logicalGroup[k] = { // less than or equal
+            le: [
+              parts[0],
+              parts[1]
+            ]
+          };
+        } else if (__contains(logicalGroup[k], LT)) {
+          var parts = __splitTrim(logicalGroup[k], LT);
+          logicalGroup[k] = { // less than
+            lt: [
+              parts[0],
+              parts[1]
+            ]
+          };
+        } else if (__contains(logicalGroup[k], EQUAL)) {
+          var parts = __splitTrim(logicalGroup[k], EQUAL);
+          logicalGroup[k] = {
+            eq: [
+              parts[0],
+              parts[1]
+            ]
+          };
         }
+      }
     }
+  }
 }
 
 function __findIndex(str, token) {
-    var index = str.indexOf(token);
-    return index === -1 ? Number.MAX_VALUE : index;
+  var index = str.indexOf(token);
+  return index === -1 ? Number.MAX_VALUE : index;
 }
 function __splitTrim(str, token) {
-    return str.split(token).map(function (p) {
-        return p.trim();
-    });
+  return str.split(token).map(function (p) {
+    return p.trim();
+  });
 }
 function __contains(a, b) {
-    return a.indexOf(b) > -1;
+  return a.indexOf(b) > -1;
 }
 function __hierarchize(obj, dottedPath) {
-    var parts = __splitTrim(dottedPath, DELIMITER);
-    var res = obj;
-    for (var p in parts)
-        if (res.hasOwnProperty(parts[p]))
-            res = res[parts[p]];
-        else
-            return '';
-    return res.toString();
+  var parts = __splitTrim(dottedPath, DELIMITER);
+  var res = obj;
+  for (var p in parts) {
+    if (res.hasOwnProperty(parts[p]))
+      res = res[parts[p]];
+    else
+      return '';
+  }
+  return res.toString();
 }
 
 function FilterOR(ASTNode, row) {
-    var res = false;
-    for (var k in ASTNode) {
-        var filterFunc = (k === AND_STR ? FilterAND : (k === OR_STR ? FilterOR : Filter));
-        res = res || filterFunc(ASTNode[k], row);
-        if (options.trace)
-            console.log(synopsis.step, '======((( or', ASTNode[k], res);
-        if (res) return res;
-    }
-    return res;
+  var res = false;
+  for (var k in ASTNode) {
+    var filterFunc = (k === AND_STR ? FilterAND : (k === OR_STR ? FilterOR : Filter));
+    res = res || filterFunc(ASTNode[k], row);
+    if (options.trace)
+      print(synopsis.step, '======((( or', ASTNode[k], res);
+    if (res) return res;
+  }
+  return res;
 }
 function FilterAND(ASTNode, row) {
-    var res = true;
-    for (var k in ASTNode) {
-        var filterFunc = (k === AND_STR ? FilterAND : (k === OR_STR ? FilterOR : Filter));
-        res = res && filterFunc(ASTNode[k], row);
-        if (options.trace)
-            console.log(synopsis.step, '======((( and', ASTNode[k], res);
-        if (!res) return res;
-    }
-    return res;
+  var res = true;
+  for (var k in ASTNode) {
+    var filterFunc = (k === AND_STR ? FilterAND : (k === OR_STR ? FilterOR : Filter));
+    res = res && filterFunc(ASTNode[k], row);
+    if (options.trace)
+      print(synopsis.step, '======((( and', ASTNode[k], res);
+    if (!res) return res;
+  }
+  return res;
 }
 function Filter(ASTNode, row) {
-    synopsis.step += 1;
-    if (ASTNode.or) {
-        var res = FilterOR(ASTNode.or, row);
-        if (options.trace)
-            console.log(synopsis.step, 'OR', ASTNode, res);
-        return res;
-    } else if (ASTNode.and) {
-        var res = FilterAND(ASTNode.and, row);
-        if (options.trace)
-            console.log(synopsis.step, 'AND', ASTNode, res);
-        return res;
-    } else if (typeof ASTNode === 'object') {
-        if (ASTNode.eq) { // =
-            return __hierarchize(row, ASTNode.eq[0]) === ASTNode.eq[1];
-        } else if (ASTNode.ne) { // !=
-            return __hierarchize(row, ASTNode.ne[0]) !== ASTNode.ne[1];
-        } else if (ASTNode.req) { // ~
-            return __contains(__hierarchize(row, ASTNode.req[0]), ASTNode.req[1]);
-        } else if (ASTNode.nreq) { // ~
-            return !__contains(__hierarchize(row, ASTNode.nreq[0]), ASTNode.nreq[1]);
-        } else {
-            return Filter(ASTNode, row);
-        }
+  synopsis.step += 1;
+  if (ASTNode.or) {
+    var res = FilterOR(ASTNode.or, row);
+    if (options.trace)
+      print(synopsis.step, 'OR', ASTNode, res);
+    return res;
+  } else if (ASTNode.and) {
+    var res = FilterAND(ASTNode.and, row);
+    if (options.trace)
+      print(synopsis.step, 'AND', ASTNode, res);
+    return res;
+  } else if (typeof ASTNode === 'object') {
+    if (ASTNode.eq) { // =
+      return __hierarchize(row, ASTNode.eq[0]) === ASTNode.eq[1];
+    } else if (ASTNode.ne) { // !=
+      return __hierarchize(row, ASTNode.ne[0]) !== ASTNode.ne[1];
+    } else if (ASTNode.req) { // ~
+      return __contains(__hierarchize(row, ASTNode.req[0]), ASTNode.req[1]);
+    } else if (ASTNode.nreq) { // ~
+      return !__contains(__hierarchize(row, ASTNode.nreq[0]), ASTNode.nreq[1]);
+    } else if (ASTNode.gt) { // >
+      return __hierarchize(row, ASTNode.gt[0]) > ASTNode.gt[1];
+    } else if (ASTNode.ge) { // >=
+      return __hierarchize(row, ASTNode.ge[0]) >= ASTNode.ge[1];
+    } else if (ASTNode.lt) { // <
+      return __hierarchize(row, ASTNode.lt[0]) < ASTNode.lt[1];
+    } else if (ASTNode.le) { // <=
+      return __hierarchize(row, ASTNode.le[0]) <= ASTNode.le[1];
+    } else {
+      return Filter(ASTNode, row);
     }
+  }
 }
 function Parse(dataSource) {
-    var result = [];
-    for (var k in dataSource)
-        if (Filter(AST, dataSource[k]))
-            result.push(dataSource[k]);
-    return result;
+  var result = [];
+  for (var k in dataSource)
+    if (Filter(AST, dataSource[k]))
+      result.push(dataSource[k]);
+  return result;
 }
 function Fields(result) {
-    if (result && synopsis.pathway.length > 0) {
-        //console.log(synopsis.pathway);
-        return result.map(function (ele) {
-            var res = {};
-            for(var i = 0, len = synopsis.pathway.length; i< len; i++){
-                var key  = synopsis.pathway[i].join(DELIMITER);
-                res[key] =  __hierarchize(ele, key);
-            }
-            return res;
-        });
-    }
-    return result;
+  if (result && synopsis.pathway.length > 0) {
+    //print(synopsis.pathway);
+    return result.map(function (ele) {
+      var res = {};
+      for (var i = 0, len = synopsis.pathway.length; i < len; i++) {
+        var key = synopsis.pathway[i].join(DELIMITER);
+        res[key] = __hierarchize(ele, key);
+      }
+      return res;
+    });
+  }
+  return result;
 }
 function Query(dataSource, query, opts) {
-    synopsis = {
-        pathway: [],
-        groups: {},
-        step: 0
+  synopsis = {
+    pathway: [],
+    groups: {},
+    step: 0
+  };
+  AST = {};
+  opts = opts || {
+      trace: false
     };
-    AST = {};
-    opts = opts || {
-        trace: false
-    };
-    options = opts;
-    Tokenize(query);
-    return Fields(Parse(dataSource));
+  options = opts;
+  Tokenize(query);
+  return Fields(Parse(dataSource));
 }
 
 if (typeof(module) != 'undefined' && typeof(module.exports) != 'undefined') module.exports = Query;
